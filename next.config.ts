a4d192f4c61be9347b324d338f7e8c06b9b1e4e8
Kao Vannak <kaovannak009@gmail.com>
2025-02-02 18:07:54 +0700

    Updated deploy settings

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@
 const nextConfig = {
   output: "export",
   distDir: "out",
-  basePath: "/fasy-midterm",  // Change to your repo name
+  basePath: "/fasy-midterm",  // Your GitHub repo name
   assetPrefix: "/fasy-midterm/",
 };
 
 module.exports = nextConfig;
-
